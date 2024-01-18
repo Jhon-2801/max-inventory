5bd4366b851b5fa80fc8aaca554de1dd3f798def
Jhon-2801 <jangulovides@gmail.com>
2024-01-18 15:25:18 -0500

    agregar squema.sql

---
+++ internal/db/squema.sql
@@ -0,0 +1,39 @@
+create database max_inventory;
+use max_inventory;
+
+create table USERS (
+    id int not null auto_increment,
+    mail varchar(255) not null, 
+    name varchar(50) not null,
+    password varchar(50) not null,
+    primary key(id)
+);
+
+create table PRODUCTS(
+    id int not null auto_increment,
+    description varchar(255) not null, 
+    name varchar(50) not null,
+    price float not null,
+    create_by int not null,
+    primary key(id),
+    foreign key (create_by) references USERS(id)
+);
+
+create table ROLES(
+    id not null auto_increment,
+    name varchar(255) not null,
+    primary key(id)
+);
+
+create table USER_ROLES(
+    id int not null auto_increment,
+    user_id int not null, 
+    role_id int not null,
+    primary key(id),
+    foreign key (user_id) references USERS(id),
+    foreign key (role_id) references ROLES(id)
+);
+
+insert into ROLES(name) values ('admin')
+insert into ROLES(name) values ('seller')
+insert into ROLES(name) values ('customer')
